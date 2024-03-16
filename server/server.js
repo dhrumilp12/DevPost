d47b09d47f427fa0891178a01b8cee47c65b2859
Sahil Patel <sahildepatel@gmail.com>
2024-03-16 16:09:17 -0400

    Starting the server file and getting serverside ready

--- server/server.js
+++ server/server.js
@@ -1,32 +1,16 @@
-const require = require('express');
+const express = require('express');
+const routes = require('./routes');
+// import sequelize connection
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-const route = require('./routes');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(route);
+app.use(routes);
 
+// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-    
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-      
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
-    
-    db.once('open', () => {
-        app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        })
-    })
-    };
+  console.log(`App listening on port ${PORT}!`);
+});
